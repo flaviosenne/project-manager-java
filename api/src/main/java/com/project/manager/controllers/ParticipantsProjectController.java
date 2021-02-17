8c9cf895af75c852c203a0e1b841cd2a2f95c3ae
flaviosenne <flaviosenne123@gmail.com>
2021-02-17 10:52:31 -0300

    config domain references table

--- api/src/main/java/com/project/manager/controllers/ParticipantsProjectController.java
+++ api/src/main/java/com/project/manager/controllers/ParticipantsProjectController.java
@@ -5,6 +5,8 @@ import com.project.manager.services.ParticipantsProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,10 @@ public class ParticipantsProjectController {
     @GetMapping
     public ResponseEntity<List<ParticipantsProject>> getAll(){
         return ResponseEntity.status(200).body(this.participantsProjectService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<ParticipantsProject> create(@RequestBody ParticipantsProject participants){
+        return ResponseEntity.status(201).body(this.participantsProjectService.create(participants));
     }
 }
